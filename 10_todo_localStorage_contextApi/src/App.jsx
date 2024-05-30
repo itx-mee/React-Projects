a909362d71be90fc4b554030603f273f38ad005c
Shikha Maurya <141204585+itx-mee@users.noreply.github.com>
2024-05-30 06:12:03 +0000

    UI improvements

--- 10_todo_localStorage_contextApi/src/App.jsx
+++ 10_todo_localStorage_contextApi/src/App.jsx
@@ -42,8 +42,8 @@ function App() {
 
   return (
     <TodoProvider value = { { todos, addTodo, updateTodo, deleteTodo, toggleTodo } }>
-      <h1 className='bg-slate-300 text-2xl'>TODO Project</h1>
       <div className="bg-[#172842] min-h-screen py-8">
+      <h1 className='bg-slate-300 text-2xl text-center text-3xl'>TODO Project</h1>
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
--- 11_todo_RTK/src/components/Todo.jsx
+++ 11_todo_RTK/src/components/Todo.jsx
@@ -20,17 +20,17 @@ function Todo() {
 
   return (
     <div className="bg-gray-200 w-full h-screen p-14">
-      <form onSubmit={onSubmitHandler} className="space-x-3 mt-12">
+      <form onSubmit={onSubmitHandler} className="w-full flex space-x-3 mt-12">
         <input
           type="text"
-          className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          className="w-10/12 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           placeholder="Enter a Todo..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
         <button
           type="submit"
-          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          className=" w-2/12 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
         >
           Add Todo
         </button>
